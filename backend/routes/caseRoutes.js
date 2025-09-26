27363416551e3b373582ad4b48b4e62ba5ae20c6
Ace072293 <dostacedy@gmail.com>
2025-09-26 22:06:09 +1000

    observer pattern integration

--- backend/routes/caseRoutes.js
+++ backend/routes/caseRoutes.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const { getCases, createCase, updateCaseStatus, deleteCase } = require('../controllers/CaseController');
+const { getCases, createCase, updateCaseStatus, deleteCase, assignLawyerToCase } = require('../controllers/CaseController');
 const { protect } = require('../middleware/authMiddleware');
 const { validateCaseCreation } = require('../middleware/validationMiddleware');
 
@@ -8,7 +8,8 @@ const router = express.Router();
 
 router.post('/', protect, validateCaseCreation, createCase);
 router.get('/', protect, getCases);
-router.patch('/:id', protect, updateCaseStatus);
+router.patch('/:id/status', protect, updateCaseStatus);
+router.patch('/:id/assign', protect, assignLawyerToCase); // New route for assigning a lawyer
 router.delete('/:id', protect, deleteCase);
 
 module.exports = router;
