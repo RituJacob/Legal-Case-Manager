569609e2b1855486ca94a7f0c51eb567686cda65
RituJacob <ritujacob2003@gmail.com>
2025-09-27 15:01:19 +1000

    minor fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -24,12 +24,7 @@ const Navbar = () => {
           >
             Cases
           </Link>
-          <Link
-            to="/storage"
-            className="px-3 py-2 rounded-md hover:bg-blue-600 transition-colors"
-          >
-            Storage
-          </Link>
+         
           <Link
             to="/profile"
             className="px-3 py-2 rounded-md hover:bg-blue-600 transition-colors"
